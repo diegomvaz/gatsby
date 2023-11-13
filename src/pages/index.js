ac8b0a98be5fdd9ba5cd893c3c758e75f3942d98
diegomvaz <martins13@hotmail.com>
2023-11-12 21:48:40 -0300

    teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => {
       <h1>Olá Mundo Gatsby</h1>
       <div>
         <p>Esse é o primeiro parágrafo</p>
-        <p>Esse é o segundo parágrafo</p>
+        <p>Esse é o segundo parágrafo</p> 
       </div>
     </>
   )
